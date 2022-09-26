86297d89c4026ead808a2bff47fab091acc927d5
DongGukMon <30457954+DongGukMon@users.noreply.github.com>
2022-09-26 18:22:34 +0900

    Delete constants.js

--- src/constants.js
+++
@@ -1,7 +0,0 @@
-export const API_KEY = "AIzaSyDiKbD5nKPzLNKQ2i8zHKHR6VHn1AYjlis";
-export const AUTH_DOMAIN = "zolzac-60337.firebaseapp.com";
-export const PROJECT_ID = "zolzac-60337";
-export const STORAGE__BUCKET = "zolzac-60337.appspot.com";
-export const MESSAGING_SENDER_ID = "391665152004";
-export const APP_ID = "1:391665152004:web:0ef3da2ae6338085544551";
-export const MEASUERMENT_ID = "G-L639YSSKJT";
